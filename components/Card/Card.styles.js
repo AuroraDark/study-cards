ac54546298f458cda1c381ce6b29f1f4fb494e5b
AuroraDark <andressa.workti@gmail.com>
2022-04-20 01:14:05 -0300

    Tela de Criação de Card

--- components/Card/Card.styles.js
+++ components/Card/Card.styles.js
@@ -1,56 +1,43 @@
-import { StyleSheet } from 'react-native';
-import { vw, vh, vmin, vmax } from 'react-native-expo-viewport-units';
-
+import { StyleSheet } from "react-native";
+import { vw, vh, vmin, vmax } from "react-native-expo-viewport-units";
 
 export const styles = StyleSheet.create({
-    cardBody: {
-      flex: 1,
-      width: vw(80),
-      height: vh(65),
-      borderRadius: 40,
-      padding: 20,
-      marginLeft: vw(3),
-      marginRight: vw(3),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center'
-    },
+  cardBody: {
+    flex: 1,
+    width: vw(80),
+    height: vh(65),
+    borderRadius: 40,
+    padding: 20,
+    marginLeft: vw(3),
+    marginRight: vw(3),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
 
-    scrollView: {
-        marginTop: '5%',
-        height: '82%',
-    },
+  scrollView: {
+    marginTop: "5%",
+    height: "82%",
+  },
 
-    cardTitulo: {
-        marginTop: 40,
-        color: '#f2f2f2',
-        textAlign: 'center',
-        fontFamily: 'Epilogue_500Medium',
-        fontSize: 30,
-    },
+  cardTitulo: {
+    marginTop: 40,
+    color: "#f2f2f2",
+    textAlign: "center",
+    fontFamily: "Epilogue_500Medium",
+    fontSize: 30,
+  },
 
-    dica: {
-        marginTop: 20,
-        textAlign: 'center',
-        color: '#f2f2f2',
-        fontFamily: 'Epilogue_500Medium',
-        fontSize: 18,
-    },
+  dica: {
+    marginTop: 20,
+    textAlign: "center",
+    color: "#f2f2f2",
+    fontFamily: "Epilogue_500Medium",
+    fontSize: 18,
+  },
 
-    lampIcon: {
-        position: 'absolute',
-        bottom: 30,
-        left:30,
-    },
-
-    starIcon: {
-        position: 'absolute',
-        bottom: 30,
-        right:30,
-    },
-    
-    elevation: {
-        elevation: 15,
-        shadowColor: '#121212',
-      },
-  });
+  elevation: {
+    elevation: 15,
+    shadowColor: "#121212",
+  },
+});
